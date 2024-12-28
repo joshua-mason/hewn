d77a1ebc206e286b8d3a5e033baf60b6824ac916
Joshua Mason <jmmason95@googlemail.com>
2024-12-28 20:30:34 +0000

    Basic repetitive jumping impl

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use game::Direction;
 use game::Game;
 use termion;
 
+const WIDTH: usize = 10;
+const HEIGHT: usize = 20;
+const FRAME_RATE_MILLIS: u64 = 100;
+const SCREEN_HEIGHT: u16 = 20;
+
 fn main() {
     let (stdout, stdin) = io::initialize_terminal();
-    let mut game = game::Game { player_position: 0 };
+    let mut game = game::Game::new(WIDTH, HEIGHT);
     let mut control = control::Control {
         stdin,
         game: &mut game,
@@ -30,7 +35,11 @@ fn main() {
 
 mod game {
     pub struct Game {
-        pub player_position: usize,
+        pub player_pos_x: usize,
+        pub player_pos_y: isize,
+        pub player_velocity: isize,
+        pub width: usize,
+        pub height: usize,
     }
 
     pub enum Direction {
@@ -39,15 +48,33 @@ mod game {
     }
 
     impl Game {
+        pub fn new(width: usize, height: usize) -> Game {
+            Game {
+                player_pos_x: 1,
+                player_pos_y: 1,
+                player_velocity: 5,
+                width,
+                height,
+            }
+        }
+
         pub fn player_move(&mut self, direction: Direction) {
             match direction {
-                Direction::Left if self.player_position != 0 => {
-                    self.player_position = self.player_position - 1;
+                Direction::Left if self.player_pos_x != 0 => {
+                    self.player_pos_x = self.player_pos_x - 1;
                 }
-                Direction::Right if self.player_position < 5 => {
-                    self.player_position = 1 + self.player_position;
+                Direction::Right if self.player_pos_x < self.width - 1 => {
+                    self.player_pos_x = 1 + self.player_pos_x;
                 }
                 _ => {}
+            }
+        }
+
+        pub fn next(&mut self) {
+            self.player_velocity -= 1;
+            self.player_pos_y += self.player_velocity;
+            if self.player_pos_y <= 1 {
+                self.player_velocity = 5;
             }
         }
     }
@@ -59,6 +86,8 @@ mod display {
     use termion::raw::RawTerminal;
 
     use crate::game::Game;
+    use crate::SCREEN_HEIGHT;
+    use crate::WIDTH;
 
     pub struct Display {
         stdout: RawTerminal<Stdout>,
@@ -68,11 +97,12 @@ mod display {
         pub fn next(&mut self, game: &Game) {
             write!(
                 self.stdout,
-                "{}{}{}",
+                "{}{}{}{}{}",
                 termion::clear::All,
                 termion::cursor::Goto(1, 1),
-                Display::stringify_game(game),
-                // key
+                Display::player_level(game),
+                termion::cursor::Goto(1, SCREEN_HEIGHT + 2),
+                Display::debug(game),
             )
             .unwrap();
 
@@ -83,17 +113,37 @@ mod display {
             Display { stdout }
         }
 
-        fn stringify_game(game: &Game) -> String {
-            let mut s = "......".to_owned();
+        fn debug(game: &Game) -> String {
+            format!(
+                "v = {:4}, x = {:3}, y = {:3}",
+                game.player_velocity, game.player_pos_x, game.player_pos_y
+            )
+        }
 
-            s.replace_range(game.player_position..(game.player_position + 1), "#");
-            s
+        fn player_level(game: &Game) -> String {
+            let mut display_string = "".to_owned();
+            for height in 0..(SCREEN_HEIGHT) {
+                let mut level = Display::build_level_string();
+                if ((SCREEN_HEIGHT - height) == game.player_pos_y as u16) {
+                    level.replace_range(game.player_pos_x..(game.player_pos_x + 1), "#");
+                }
+                level.push_str(&termion::cursor::Goto(1, height).to_string());
+                display_string.push_str(&level);
+            }
+            display_string
+        }
+
+        fn build_level_string() -> String {
+            let whitespaces = std::iter::repeat('.').take(WIDTH);
+            let whitespaces = Vec::from_iter(whitespaces);
+            let level = String::from_iter(whitespaces);
+            level
         }
     }
 }
 
 mod control {
-    use crate::game::Game;
+    use crate::{game::Game, FRAME_RATE_MILLIS};
     use std::{thread, time};
 
     pub struct Control<'a> {
@@ -118,8 +168,9 @@ mod control {
                         }
                     }
                 }
-                thread::sleep(time::Duration::from_millis(50));
+                thread::sleep(time::Duration::from_millis(FRAME_RATE_MILLIS));
                 on_render(self.game);
+                self.game.next();
             }
         }
     }
